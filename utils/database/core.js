ad43c1326928547a48e9935e6d2b7a0b1ba535e7
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-02 19:39:37 +0500

    update

--- utils/database/core.js
+++ utils/database/core.js
@@ -16,3 +16,21 @@ pool.query('SELECT NOW()', (err, res) => {
         console.log('Connected to database. Current time in the database:', res.rows[0].now);
     }
 });
+
+// Запрос для получения списка всех таблиц в базе данных
+const getTablesQuery = `
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+    AND table_type = 'BASE TABLE';
+`;
+
+// Выполнение запроса для получения списка всех таблиц
+pool.query(getTablesQuery, (err, res) => {
+    if (err) {
+        console.error('Ошибка при получении списка таблиц:', err);
+    } else {
+        const tables = res.rows.map(row => row.table_name);
+        console.log('Имена таблиц в базе данных:', tables);
+    }
+});
