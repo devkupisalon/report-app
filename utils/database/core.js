04ac6a3f777cd934dd73d0380d60511f33088135
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-03 11:03:11 +0500

    update

--- utils/database/core.js
+++ utils/database/core.js
@@ -64,8 +64,9 @@ const formatUserFiles = (users, files) => {
 
     files.forEach(file => {
         const fileDate = new Date(file.uploaded_to_telegram_at);
+        console.log(fileDate.toDateString());
 
-        if (new Date(fileDate).toDateString() === previousDay.toDateString() && usersMap[file.user_id]) {
+        if (fileDate.toDateString() === previousDay.toDateString() && usersMap[file.user_id]) {
             usersMap[file.user_id].user_files.push({
                 type: file.media_type === 1 ? 'Видео' : 'Фото',
                 tg_id: file.tg_file_id
