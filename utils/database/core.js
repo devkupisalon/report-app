313441013f91c1ce60d114d4160f5a5e9deda098
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-03 10:47:46 +0500

    update

--- utils/database/core.js
+++ utils/database/core.js
@@ -49,7 +49,7 @@ pool.query('SELECT * FROM files', (err, res) => {
     if (err) {
         console.error('Ошибка при выполнении запроса:', err);
     } else {
-        console.table(res.rows);
+        console.log(res.rows);
     }
 });
 
