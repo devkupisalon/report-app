ba57f5537a2ff2f3dbaced1927234e57dc002d58
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-05 22:42:43 +0500

    update

--- utils/database/files.js
+++ utils/database/files.js
@@ -41,7 +41,7 @@ const get_data_for_report = async () => {
 const get_first_10_logs = async () => {
     return new Promise((resolve, reject) => {
         const yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 1);
+        // yesterday.setDate(yesterday.getDate() - 1);
         const yesterdayDate = yesterday.toISOString().split('T')[0];
 
         pool.query(`SELECT (files.file_id, files.name, files.media_type, files.uploaded_to_telegram_at, files.uploaded_to_yandex_at, files.uploaded_to_google_at, users.tg_username, file_links.storage_type, file_links.file_link, file_links.directory_name, file_links.directory_link) 
