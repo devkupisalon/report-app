120d10461c320e06fb21c678d47b554bddf27fe1
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-07 01:45:01 +0500

    update

--- server.js
+++ server.js
@@ -51,8 +51,3 @@ app.listen("8000", "localhost", (err) => {
     }
     logger.info("Server is running on port 8000", { module });
 });
-
-
-// path to get download link from yandex disk
-// https://cloud-api.yandex.net/v1/disk/resources/download?path=disk%3A%2F%D0%9A%D1%83%D0%BF%D0%B8%20%D1%81%D0%B0%D0%BB%D0%BE%D0%BD%2F%D0%97%D0%B0%D0%BF%D0%B8%D1%81%D0%B8%20%D0%B7%D0%B2%D0%BE%D0%BD%D0%BA%D0%BE%D0%B2%2F25-09-2023-15%3A16%3A01-79623279352-%D0%93%D0%B2%D0%B0%D1%81%D0%B0%D0%BB%D0%B8%D1%8F%20%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%BE%D0%B7
-// disk:/Купи салон/Записи звонков/25-09-2023-15:16:01-79623279352-Гвасалия Николоз
