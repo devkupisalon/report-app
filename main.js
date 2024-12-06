b49ab9b0fede29b67ea38dc5f2743a272361af0a
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-06 20:42:19 +0500

    update

--- main.js
+++ main.js
@@ -14,18 +14,14 @@ const module = import.meta.filename;
 const data_for_web_app = async () => {
     try {
         let i = 0;
-        let exist;
         const data = await get_files_data();
         const users = await get_users_data();
         const result = {};
         for (const [k, v] of Object.entries(data)) {
             for (const { id, date, type, username, link, path } of Object.values(data[k])) {
-                if (exist !== path.split('/').pop()) {
-                    const url = await get_download_link(path);
-                    const name = find_name_by_username(username, users);
-                    result[i] = { name, date, type, url, yes: 'FALSE', no: 'FALSE', comment: '', link };
-                }
-                exist = path.split('/').pop();
+                const url = await get_download_link(path);
+                const name = find_name_by_username(username, users);
+                result[i] = { name, date, type, url, yes: 'FALSE', no: 'FALSE', comment: '', link };
                 i++;
             }
         }
