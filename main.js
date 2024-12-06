57765524d0fa416feca10bdc4a6906ae13797cf4
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-06 19:23:26 +0500

    update

--- main.js
+++ main.js
@@ -11,21 +11,26 @@ const module = import.meta.filename;
  * оператор	    дата загрузки	        тип контента	   сылка на контент	Принято	Отклонено	Комментарий
  *   Даша	   18.11.2024 18:32:34	 type photo or video	        link         FALSE	   FALSE	some comment
  */
-const data_for_web_app = async () => {
-    try {
-        return Object.entries(data).reduce(async (x, [k, v]) => {
-            return Object.values(data[k]).reduce(async (acc, { id, date, type, username, link, path }, i) => {
-                const url = await get_download_link(path);
-                logger.info(url);
-                const name = find_name_by_username(username, users);
-                acc[i] = { name, date, type, url, yes: 'FALSE', no: 'FALSE', comment: '', link };
-                return acc;
+const data_for_web_app = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            const result = Object.entries(data).reduce(async (x, [k, v]) => {
+                return Object.values(data[k]).reduce(async (acc, { id, date, type, username, link, path }, i) => {
+                    const url = await get_download_link(path);
+                    logger.info(url);
+                    const name = find_name_by_username(username, users);
+                    acc[i] = { name, date, type, url, yes: 'FALSE', no: 'FALSE', comment: '', link };
+                    return acc;
+                }, {});
             }, {});
-        }, {});
-    } catch (error) {
-        logger.error(`Error in data_for_web_app: ${error}`, { module });
-    }
+            resolve(result);
+        } catch (error) {
+            logger.error(`Error in data_for_web_app: ${error}`, { module });
+            reject(error);
+        }
+    });
 };
+
 const x = await data_for_web_app();
 logger.debug(x, { module });
 
