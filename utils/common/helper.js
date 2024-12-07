5428991986de15f979ec41560ee2d61f627a46b4
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-07 12:02:36 +0500

    update

--- utils/common/helper.js
+++ utils/common/helper.js
@@ -81,7 +81,7 @@ const prepare_obj = (obj) => {
   return Object.values(obj).reduce((acc, { row }, i) => {
     const elements = row.split(",");
     let [id, , type, date, , , username, , link, path] = elements;
-    type = type === 1 ? 'Видео' : 'Фото';
+    type = type === 0 ? 'Видео' : 'Фото';
     id = id.split(',')[0].substring(1);
     date = date.toString().replaceAll('"', '');
     path = path !== '' ? path.toString().replaceAll('"', '') : path;
