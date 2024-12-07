64c11f9d69625a296e03fa36e5a04f0ebade736d
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-07 11:10:38 +0500

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((error, req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+    res.header('Access-Control-Allow-Origin', 'http://172.20.10.6:8081');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
