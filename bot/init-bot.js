8fee8a427e16d1ff92b59dd321c27c1a0a41803f
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-08 00:33:41 +0500

    update

--- bot/init-bot.js
+++ bot/init-bot.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
-import { constants } from '../../constants.js';
+import { constants } from './constants.js';
 
 const { BOT_TOKEN } = constants;
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
