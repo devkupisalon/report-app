e5cc2298a352a033f87752ea98852259001840a2
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-13 00:34:30 +0500

    update

--- src/core/main.js
+++ src/core/main.js
@@ -21,30 +21,31 @@ const get_data_for_web_app = async () => {
         const data = await get_files_data();
         const users = await get_users_data();
         const result = {};
-        logger.debug(JSON.stringify(data, null, 2));
         for (const [k, v] of Object.entries(data)) {
             for (const { id, date, type, username, yandex_link, yandex_path } of Object.values(data[k])) {
-                const id = await get_download_link(yandex_path) || '';
-                const name = find_name_by_username(username, users);
-                const file_name = `${name}_${yandex_path}`;
-                const mime_type = type === 'Фото' ? 'image/png' : yandex_path.split('.')[1] === 'MOV' ? 'application/octet-stream' : 'video/mp4';
-                const { file_id, duration } = await upload_file_to_drive(id, file_name, mime_type, type);
-                if (id !== '') obj[i] = { id: file_id };
-                const google_url = type === 'Фото' ? IMAGE_LINK(file_id) : VIDEO_LINK(file_id);
-                if (id !== '') {
-                    result[i] = {
-                        name,
-                        date,
-                        username,
-                        type: type === `Фото` ? type : duration >= long ? 'Длинное видео' : 'Короткое видео',
-                        google_url,
-                        accept: 'FALSE',
-                        reject: 'FALSE',
-                        comment: '',
-                        yandex_link,
-                        yandex_path
-                    };
-                    i++;
+                if (username !== 'hungryking') {
+                    const id = await get_download_link(yandex_path) || '';
+                    const name = find_name_by_username(username, users);
+                    const file_name = `${name}_${yandex_path}`;
+                    const mime_type = type === 'Фото' ? 'image/png' : yandex_path.split('.')[1] === 'MOV' ? 'application/octet-stream' : 'video/mp4';
+                    const { file_id, duration } = await upload_file_to_drive(id, file_name, mime_type, type);
+                    if (id !== '') obj[i] = { id: file_id };
+                    const google_url = type === 'Фото' ? IMAGE_LINK(file_id) : VIDEO_LINK(file_id);
+                    if (id !== '') {
+                        result[i] = {
+                            name,
+                            date,
+                            username,
+                            type: type === `Фото` ? type : duration >= long ? 'Длинное видео' : 'Короткое видео',
+                            google_url,
+                            accept: 'FALSE',
+                            reject: 'FALSE',
+                            comment: '',
+                            yandex_link,
+                            yandex_path
+                        };
+                        i++;
+                    }
                 }
             }
         }
